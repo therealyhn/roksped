8693535b0a028355720abfde31b51e85c0459387
Jovan Ljusic <jovanljusicum@outlook.com>
2024-12-27 11:47:02 +0100

    added loader

--- script/script.js
+++ script/script.js
@@ -21,3 +21,12 @@ window.addEventListener('scroll', () => {
         navBar.classList.remove('backdrop-blur-md', 'shadow-lg', 'bg-opacity-80');
     }
 });
+
+// loader
+window.addEventListener('load', function() {
+    const loader = document.getElementById('loader-wrapper');
+    loader.style.opacity = '0';
+    setTimeout(() => {
+        loader.style.display = 'none';
+    }, 500);
+});
